2bd2f74c572161b23fb54ad4c6f78f7a5a97e090
Pedro Petroli <ppetroli@gmail.com>
2015-01-13 18:17:25 -0200

    Ajuste final do Dropbox

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,11 @@
 class House < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-					:storage => :dropbox,
-					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  	if Rails.env.development?
+  	has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+	else
+  	has_attached_file :image, :styles => { :medium => "250x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+					  :storage => :dropbox,
+					  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+					  :path => "style/:id_:filename"
+  	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	end
 end
