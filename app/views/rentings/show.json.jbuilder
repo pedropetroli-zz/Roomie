8a867163349f7496eb96bc85079f571871b17cda
Pedro Petroli <ppetroli@gmail.com>
2015-02-02 02:06:08 -0200

    Limit rentals views and access

--- app/views/rentings/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @renting, :id, :arrival, :departure, :insurance, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :houses do
-    resources :rentings
+    resources :rentings, only: [:new, :create]
   end
 
   root 'houses#index'
